891a7f0a0c1286c5de84140f759012fbb7f456cd
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2022-09-10 18:36:30 +0300

    Update 30_tasks_scheduler.py

--- Route_256-Ozon/30_tasks_scheduler.py
+++ Route_256-Ozon/30_tasks_scheduler.py
@@ -1,4 +1,4 @@
-import operator
+# import operator
 
 
 class Proc:
@@ -8,8 +8,8 @@ class Proc:
         self.total_time_load = 0
         self.current_task_remaining_time = 0
 
-    def __repr__(self):
-        return str(self.rate)
+    # def __repr__(self):
+    #     return str(self.rate)
 
     def __eq__(self, other):
         return self.rate == other.rate
@@ -44,40 +44,61 @@ class Proc:
         else:
             self.total_time_load += self.current_task_remaining_time
             self.current_task_remaining_time = 0
+            return True
 
 
 class ProcPool:
     def __init__(self, procs):
-        procs.sort()  # key=operator.attrgetter('current_task_remaining_time', 'rate')
+        # procs.sort()  # key=operator.attrgetter('current_task_remaining_time', 'rate')
         self.processors = procs
-        # self.busy_processors = []
+        # self.free_procs_num = len(self.processors)
+        self.busy_processors = []
 
-    def __repr__(self):
-        return self.processors
+    # def __repr__(self):
+    #     return self.processors
 
     def total_energy_used(self):
         res = 0
-        for proc in self.processors:
+        for proc in list(self.processors + self.busy_processors):
             res += proc.total_energy_used
         return res
 
+    # @property
+    # def busy_procs(self):
+    #     return [proc for proc in self.processors if proc.current_task_remaining_time > 0]
+
+    # @property
+    # def free_procs(self):
+    #     return [proc for proc in self.processors if proc.current_task_remaining_time == 0]
+
     def min_free_proc(self):
-        for proc in self.processors:
-            if proc.current_task_remaining_time == 0:
+        # if self.processors:
+        self.processors.sort()
+        # for proc in self.processors:
+            # if proc.current_task_remaining_time == 0:
+                # self.free_procs_num -= 1
                 # if proc not in self.busy_processors:
-                #     self.busy_processors.append(proc)
-                return proc
+        self.busy_processors.append(self.processors[0])
+        return self.processors.pop(0)
         # try:
         #     return min(proc for proc in self.processors if proc.current_task_remaining_time == 0)
         # except ValueError:
-        return False
+        # return False
 
     def busy_proc_task_proc(self, cnt=1):
-        for proc in self.processors:
-            if proc.current_task_remaining_time > 0:
-                proc.process_task(cnt)
-            # if proc.current_task_remaining_time == 0:
-            #     self.busy_processors.remove(proc)
+        list_to_remove = []
+        for proc in self.busy_processors:
+            # if proc.current_task_remaining_time > 0:
+            if proc.process_task(cnt):
+                # self.free_procs_num += 1
+        # if proc.current_task_remaining_time == 0:
+                list_to_remove.append(proc)
+        if list_to_remove:
+            # self.processors.extend(list_to_remove)
+            # self.busy_processors = [proc for proc in self.busy_processors if proc not in list_to_remove]
+            for proc in list_to_remove:
+                self.processors.append(proc)
+                self.busy_processors.remove(proc)
 
 
 proc_count, tasks_count = map(int, input().split())
@@ -102,8 +123,9 @@ for t in range(tasks_count):
 for i, task in tasks.items():
     if i != 0:
         free_proc_pool.busy_proc_task_proc(task[0] - tasks[i-1][0])
-    free_proc = free_proc_pool.min_free_proc()
-    if free_proc:
+    if len(free_proc_pool.processors) > 0:
+        free_proc = free_proc_pool.min_free_proc()
+        # if free_proc:
         free_proc.current_task_remaining_time = task[1]
         # if free_proc not in busy_proc_pool.processors:
         #     busy_proc_pool.processors.append(free_proc)
