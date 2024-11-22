9e37fee171852c4e79c5511705f4c5eabcee0bdc
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2024-11-22 17:37:52 +0300

    Restructure and updating

--- Python_Intro-GB/helloworld/14-Instr/14-1-Instr.py
+++
@@ -1,5 +0,0 @@
-fruits1 = ['apple', 'peach', 'lemon', 'raspberry', 'pineapple']
-fruits2 = ['apple', 'lemon', 'raspberry', 'strawberry', 'litchi']
-
-fruits12 = [fruit for fruit in fruits1 if fruit in fruits2]
-print(fruits12)
--- Python_Intro-GB/helloworld/14-Instr/14-2-Instr.py
+++
@@ -1,7 +0,0 @@
-import random
-
-list1 = [random.randint(-100, 100) for i in range(15)]
-print(list1)
-
-list2 = [num for num in list1 if num > 0 and num % 3 == 0 and num % 4 != 0]
-print(list2)
