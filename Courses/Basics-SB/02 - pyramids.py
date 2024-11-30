d572dd750918e86d6b6429337baa65321fb0cba3
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2024-11-30 23:39:47 +0300

    New exercises added

--- Courses/Basics-SB/02 - pyramids.py
+++ Courses/Basics-SB/02 - pyramids.py
@@ -1,3 +1,4 @@
+# Пирамида
 size = 5
 next_num = 1
 
@@ -12,3 +13,18 @@ for row in range(size):
             print(end='  ')
             last_is_num = False
     print()
+
+
+# Яма
+size = 5
+
+for row in range(size):
+    for col in range(size * 2):
+        if row < col < size * 2 - row - 1:
+            print('.', end='')
+        elif col < size:
+            print(size - col, end='')
+        else:
+            print(col + 1 - size, end='')
+    print()
+
